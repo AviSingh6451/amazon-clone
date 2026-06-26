2f267e8d3349317abcfad0e02c5beddfe8b5c2b3
Avi <6451avisingh@gmail.com>
2026-06-26 16:49:31 +0530

    Added all new routes to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,9 @@ import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Cart from './pages/Cart'
 import Checkout from './pages/Checkout'
+import ProductDetail from './pages/ProductDetail'
+import Wishlist from './pages/Wishlist'
+import OrderHistory from './pages/OrderHistory'
 
 function App() {
   return (
@@ -20,10 +23,13 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<ProductDetail />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/checkout" element={<Checkout />} />
+            <Route path="/wishlist" element={<Wishlist />} />
+            <Route path="/orders" element={<OrderHistory />} />
           </Routes>
         </BrowserRouter>
       </CartProvider>
